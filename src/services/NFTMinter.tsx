6c841072efc54c0029a3d0103f07c4b8d977f632
Tim Sukamtoh <timtoh168@gmail.com>
2024-02-28 15:26:01 -0700

    creating contract reading and writing functions

--- src/services/NFTMinter.tsx
+++ src/services/NFTMinter.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
+import { NFT_ABI } from '@/data/nfts';
+
+import { useWriteContract, useReadContract} from 'wagmi';
+import Image from 'next/image';
 
 const MintNFT = () => {
-    const handleMintNFT = async () =>{
-        try{
-            //Call mint function
-            // const nftId = await wagmiMINTNFT()
-
-        } catch (err) {
-            console.error('error minting', err)
-        }
-    };
+    const { writeContract } = useWriteContract();
+    try{
+        //Call mint function
+        // const nftId = await wagmiMINTNFT()
+         writeContract({
+            abi:NFT_ABI,
+            address:'',
+            functionNAme:'mint',
+            args: [
+                'sero',
+                'link',
+                'image',
+                'address'
+            ]
+        })
+    } catch (err) {
+        console.error('error minting', err)
+    }
+    ;
 
     return (
         <div>
             <button onClick={handleMintNFT}> Mint NFT</button>
         </div>
     )
-}
 
+};
 export default MintNFT;
